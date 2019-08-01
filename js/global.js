012f991296645d815dc014993185e5110496b74a
Wu Mingfei <31245615+wmf1993@users.noreply.github.com>
2019-08-01 18:38:34 +0800

    feat:share for wx

--- js/global.js
+++ js/global.js
@@ -309,6 +309,69 @@ function move (obj,direction) {
     }, duration)
 }
 
+$(document).ready(function () {
+    var imgUrl ='http://zzhz.zjol.com.cn/material/public-resource/img/share-logo.png';
+    
+    //获取本页面地址作为分享指向链接(重要!!!!)
+    var lineLink = window.location.href;
+    //微信分享摘要（35字以内）：必填
+    var descContent =$('.abstract').text();
+    //微信分享标题(23字以内)：必填
+    var shareTitle = document.title;
+    //对应频道的id：必填
+    var appid = '';
+    //以下内容不需要编辑
+    function shareFriend() {
+        WeixinJSBridge.invoke('sendAppMessage',{
+            "appid": appid,
+            "img_url": imgUrl,
+            "img_width": "300",
+            "img_height": "300",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+            _report('send_msg', res.err_msg);
+        })
+    }
+    function shareTimeline() {
+        WeixinJSBridge.invoke('shareTimeline',{
+            "img_url": imgUrl,
+            "img_width": "300",
+            "img_height": "300",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+            _report('timeline', res.err_msg);
+        });
+    }
+    function shareWeibo() {
+        WeixinJSBridge.invoke('shareWeibo',{
+
+            "content": descContent,
+            "url": lineLink,
+        }, function(res) {
+            _report('weibo', res.err_msg);
+        });
+    }
+    // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+        // 发送给好友
+        WeixinJSBridge.on('menu:share:appmessage', function(argv){
+            shareFriend();
+        });
+        // 分享到朋友圈
+        WeixinJSBridge.on('menu:share:timeline', function(argv){
+            shareTimeline();
+        });
+        // 分享到微博
+        WeixinJSBridge.on('menu:share:weibo', function(argv){
+            shareWeibo();
+        });
+    }, false);
+})
+
 window.onload = function () {
     navClick()
 }
